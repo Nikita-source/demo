b78ecce013cf5b32c341b48c4eeae13fdd7a336e
Nikita <logachovnik@gmail.com>
2021-09-16 11:00:08 +0300

    Add migrations

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,121 @@
+CREATE TABLE addresses (
+    id bigint NOT NULL PRIMARY KEY,
+    address character varying(255) NOT NULL
+);
+
+CREATE TABLE categories (
+    id bigint NOT NULL PRIMARY KEY,
+    title character varying(255) NOT NULL
+);
+
+CREATE TABLE customers (
+    id bigint NOT NULL PRIMARY KEY,
+    email character varying(255) NOT NULL,
+    lastname character varying(255) NOT NULL,
+    name character varying(255) NOT NULL,
+    phonenumber character varying(255) NOT NULL,
+    secondname character varying(255) NOT NULL
+);
+
+CREATE TABLE orders (
+    id bigint NOT NULL PRIMARY KEY,
+    customer_id bigint,
+    product_id bigint
+);
+
+CREATE TABLE product_categories (
+    product_id bigint NOT NULL,
+    category_id bigint NOT NULL,
+    PRIMARY KEY (product_id, category_id)
+);
+
+CREATE TABLE products (
+    id bigint NOT NULL PRIMARY KEY,
+    availability boolean,
+    description character varying(255) NOT NULL,
+    price numeric(19,2) NOT NULL,
+    title character varying(255) NOT NULL
+);
+
+CREATE TABLE roles (
+    id bigint NOT NULL PRIMARY KEY,
+    name character varying(20)
+);
+
+CREATE TABLE shopinventories (
+    id bigint NOT NULL PRIMARY KEY,
+    count bigint,
+    product_id bigint,
+    shop_id bigint
+);
+
+CREATE TABLE shops (
+    id bigint NOT NULL PRIMARY KEY,
+    title character varying(255) NOT NULL,
+    address_id bigint
+);
+
+CREATE TABLE supplies (
+    id bigint NOT NULL PRIMARY KEY,
+    count bigint,
+    date timestamp without time zone,
+    product_id bigint,
+    shop_id bigint,
+    warehouse_id bigint
+);
+
+CREATE TABLE user_roles (
+    user_id bigint NOT NULL,
+    role_id bigint NOT NULL,
+    PRIMARY KEY (user_id, role_id)
+);
+
+CREATE TABLE users (
+    id bigint NOT NULL PRIMARY KEY,
+    birthday timestamp without time zone NOT NULL,
+    email character varying(255) NOT NULL,
+    login character varying(255) NOT NULL,
+    name character varying(255) NOT NULL,
+    password character varying(255) NOT NULL,
+    phonenumber character varying(255) NOT NULL,
+    surname character varying(255) NOT NULL
+);
+
+CREATE TABLE warehouseinventories (
+    id bigint NOT NULL PRIMARY KEY,
+    count bigint,
+    product_id bigint,
+    warehouse_id bigint
+);
+
+CREATE TABLE warehouses (
+    id bigint NOT NULL PRIMARY KEY,
+    address_id bigint
+);
+
+create unique index addresses_address_uindex on addresses (address);
+create unique index shops_title_uindex on shops (title);
+create unique index users_email_uindex on users (email);
+create unique index users_phonenumber_uindex on users (phonenumber);
+create unique index users_login_uindex on users (login);
+create unique index products_title_uindex on products (title);
+create unique index customers_email_uindex on customers (email);
+create unique index customers_phonenumber_uindex on customers (phonenumber);
+create unique index categories_title_uindex on categories (title);
+
+ALTER TABLE ONLY warehouseinventories ADD CONSTRAINT warehouseinventories_products_fk FOREIGN KEY (product_id) REFERENCES products(id);
+ALTER TABLE ONLY warehouseinventories ADD CONSTRAINT warehouseinventories_warehouses_fk FOREIGN KEY (warehouse_id) REFERENCES warehouses(id);
+ALTER TABLE ONLY warehouses ADD CONSTRAINT warehouses_addresses_fk FOREIGN KEY (address_id) REFERENCES addresses(id);
+ALTER TABLE ONLY supplies ADD CONSTRAINT supplies_products_fk FOREIGN KEY (product_id) REFERENCES products(id);
+ALTER TABLE ONLY supplies ADD CONSTRAINT supplies_shops_fk FOREIGN KEY (shop_id) REFERENCES shops(id);
+ALTER TABLE ONLY supplies ADD CONSTRAINT supplies_warehouses_fk FOREIGN KEY (warehouse_id) REFERENCES warehouses(id);
+ALTER TABLE ONLY shops ADD CONSTRAINT shops_addresses_fk FOREIGN KEY (address_id) REFERENCES addresses(id);
+ALTER TABLE ONLY shopinventories ADD CONSTRAINT shopinventories_shops_fk FOREIGN KEY (shop_id) REFERENCES shops(id);
+ALTER TABLE ONLY shopinventories ADD CONSTRAINT shopinventories_products_fk FOREIGN KEY (product_id) REFERENCES products(id);
+ALTER TABLE ONLY product_categories ADD CONSTRAINT product_categories_categories_fk FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE;
+ALTER TABLE ONLY product_categories ADD CONSTRAINT product_categories_products_fk FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE;
+ALTER TABLE ONLY user_roles ADD CONSTRAINT user_roles_roles_fk FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE;
+ALTER TABLE ONLY user_roles ADD CONSTRAINT user_roles_users_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+ALTER TABLE ONLY orders ADD CONSTRAINT orders_products_fk FOREIGN KEY (product_id) REFERENCES products(id);
+ALTER TABLE ONLY orders ADD CONSTRAINT orders_customers_fk FOREIGN KEY (customer_id) REFERENCES customers(id);
+
